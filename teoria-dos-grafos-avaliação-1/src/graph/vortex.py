ae9c922128d12f7fca936bf3a61d69bb31e8c469
Ewerton Luna <ewerton.al@gmail.com>
2022-03-27 12:15:09 -0300

    Conclui primeira versão de Edge e Vortex

--- teoria-dos-grafos-avaliação-1/src/graph/vortex.py
+++ teoria-dos-grafos-avaliação-1/src/graph/vortex.py
@@ -1,10 +1,35 @@
+from typing import Dict
 from edge import Edge
 class Vortex:
-	def __init__(self, label):
+	def __init__(self, label: str):
 		self.label = label
-		self.adjacent_edges = set() # Esse atributo recebe um conjunto de arestas
+		self.adjacent_edges: Dict[str, Edge] = dict() 
 	
-	def add_adjacent_edge(self, edge):
-		if isinstance(edge, Edge):
+	def add_adjacent_edge(self, edge: Edge):
+		"""
+		Adds an adjacent edge to vortex instance	
+
+		Parameters:
+		edge (Edge): Edge object
+	
+		Returns:
+		None
+	
+		"""
+		if not isinstance(edge, Edge):
 			raise ValueError("'edge' must be an instance of Edge")
-		self.adjacent_edges.add(edge)
+
+		self.adjacent_edges[edge.label] = edge
+	
+	def __repr__(self):
+		return f'Vortex[label: "{self.label}", adjacent_edges: "{self.adjacent_edges}]"'
+
+
+# Testing
+# vortex = Vortex('1')
+# edge_1 = Edge('a', True)
+# edge_2 = Edge('b', True)
+
+# vortex.add_adjacent_edge(edge_1)
+# vortex.add_adjacent_edge(edge_2)
+# print(vortex)
