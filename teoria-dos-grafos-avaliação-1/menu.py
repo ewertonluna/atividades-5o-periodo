4f61851da86b0c7bfc2b1d06f3a9d166c6570c1d
Ewerton Luna <ewerton.al@gmail.com>
2022-03-29 16:45:47 -0300

    [WIP] Cria menu inicial

---
+++ teoria-dos-grafos-avaliação-1/menu.py
@@ -0,0 +1,8 @@
+class Menu:
+	def create_graph():
+
+		print("Entre com o número da opção p/ dizer se o grafo é direcionado ou não-direcionado:")
+		print("1) Grafo direcionado")
+		print("2) Grafo não-direcionado")
+		option = input(">>> ")
+		print(option)
