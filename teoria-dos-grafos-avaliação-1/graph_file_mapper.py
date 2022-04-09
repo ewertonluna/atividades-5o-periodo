f1b6868ccf9dc13382da6cccea5a89082d77567b
Ewerton Luna <ewerton.al@gmail.com>
2022-04-08 21:30:11 -0300

    TODO: Completar o método GraphFileMapper._has_righ_format

--- teoria-dos-grafos-avaliação-1/graph_file_mapper.py
+++ teoria-dos-grafos-avaliação-1/graph_file_mapper.py
@@ -1,4 +1,5 @@
 from graph_file_mapper_exception import GraphFileMapperException
+import os
 
 
 class GraphFileMapper:
@@ -13,13 +14,29 @@ class GraphFileMapper:
 		self.file_path = file_path
 	
 
-	def _get_validation_message(self):
+	def _get_validation_message(self, file_path: str):
 		# check if file exists
 		# check if file is empty
 		# check if file is correctly formatted
 		# check if file is missing some field
+		validation_message = ""
+		try:
+			file = open(self.file_path, 'r')
+		except FileNotFoundError as fnfe:
+			validation_message += "The file was not found. "
+		except PermissionError as pe:
+			validation_message += "The current user doesn't have permission to read the file. "
+		if self._is_file_emtpy(file_path):
+			validation_message += "The file is empty. "
+
+
+	def _is_file_emtpy(self, file_path: str):
+		return True if not os.path.getsize(file_path) else False
+	
+	# TODO: Completar esse método
+	def _has_right_format(self, file_path: str):
 		pass
 
 
-	def is_file_path_valid(self):
-		pass
+# mapper = GraphFileMapper("/Users/ewertonluna/temp/graph.txt")
+# print(mapper._is_file_emtpy("/Users/ewertonluna/temp/graph.txt"))
