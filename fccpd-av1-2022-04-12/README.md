79d638a42f0520321bbdd739aa4fbd59c9b391cd
ewertonluna <ewerton.al@gmail.com>
2022-04-13 20:52:40 -0300

    Remove av1 de fccpd

--- fccpd-av1-2022-04-12/README.md
+++
@@ -1,2 +0,0 @@
-# AV1 - Fund. de Comp. Concorrente, Paralela e Distribuída
-## Aluno: Ewerton Luna
--- fccpd-av1-2022-04-12/av1-2022-1-v2/q4/src/xyz/rafaelri/transito/ControladorCruzamento.java
+++
@@ -1,8 +0,0 @@
-package xyz.rafaelri.transito;
-
-public interface ControladorCruzamento {
-	public void entrarHorizontal();
-	public void sairHorizontal();
-	public void entrarVertical();
-	public void sairVertical();
-}
