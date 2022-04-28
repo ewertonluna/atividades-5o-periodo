2657defafbfcd75c3041775b0ca36f80b71bd0a9
Ewerton Luna <ewerton.al@gmail.com>
2022-04-28 08:55:52 -0300

    Adicionar nova atividade

---
+++ fccpd-aula18-2022-04-26/src/main/java/school/cesar/sd/messageconsumer/MessageProducer.java
@@ -0,0 +1,17 @@
+package school.cesar.sd.messageconsumer;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageProducer implements CommandLineRunner {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    public void run(String... args) throws Exception {
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend("fanout-exchange", "", "Hello from RabbitMQ!");
+    }
+}
---
+++ fccpd-aula18-2022-04-26/src/test/java/school/cesar/sd/messageconsumer/MessageconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package school.cesar.sd.messageconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
