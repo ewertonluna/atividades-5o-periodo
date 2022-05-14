8c772832e405f3ded8c9a54041a64850ed839b52
ewertonluna <ewerton.al@gmail.com>
2022-05-13 22:28:59 -0300

    Remove unused methods

--- fccpd-aula17-2022-04-28/user.py
+++ fccpd-aula17-2022-04-28/user.py
@@ -6,41 +6,30 @@ class User:
 		self.user_id = str(user_id)
 		self._create_exchange()
 		self._create_and_bind_queues(user_id)
+
+
+	def write_group_message(self, message: str):
+		connection = self.get_connection()
+		channel = connection.channel()
+		channel.basic_publish(exchange='zapzap', routing_key='group_message_key', body=message)
+		connection.close()
+
+
+	def write_direct_message(self, recipient: str,  message: str):
+		connection = self.get_connection()
+		channel = connection.channel()
+		channel.basic_publish(exchange='zapzap', routing_key=recipient, body=message)
+		connection.close()
 	
 
-	def read_direct_messages(self):
-		settings = self._get_settings()
-		url = os.environ.get('CLOUDAMQP_URL', settings['connectionUrl'])
-		params = pika.URLParameters(url)
-		connection = pika.BlockingConnection(params)
+	def read_messages(self):
+		connection = self.get_connection()
 		channel = connection.channel()
-		channel.exchange_declare(exchange='direct_message', exchange_type='direct')
-		result = channel.queue_declare(queue=self.user_queue)
-		queue_name = result.method.queue
-		channel.queue_bind(exchange='direct_message', queue=queue_name, routing_key=self.user_queue)
 
 		def callback(ch, method, properties, body):
-			print(f"Received Message: {body}")
+			print(f"Inbox Message: {body}")
 
-		channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-		channel.start_consuming()
-	
-
-	def read_group_messages(self):
-		settings = self._get_settings()
-		url = os.environ.get('CLOUDAMQP_URL', settings['connectionUrl'])
-		params = pika.URLParameters(url)
-		connection = pika.BlockingConnection(params)
-		channel = connection.channel()
-		channel.exchange_declare(exchange='group_message', exchange_type='fanout')
-		result = channel.queue_declare(queue=self.user_queue)
-		queue_name = result.method.queue
-		channel.queue_bind(exchange='group_message', queue=queue_name, routing_key=self.user_queue)
-
-		def callback(ch, method, properties, body):
-			print(f"Received Message: {body}")
-
-		channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+		channel.basic_consume(queue=self.user_id, on_message_callback=callback, auto_ack=True)
 		channel.start_consuming()
 
 
@@ -74,31 +63,6 @@ class User:
 		channel.queue_bind(exchange='zapzap', queue=queue_name, routing_key='group_message_key')
 		connection.close()
 	
-
-	def write_group_message(self, message: str):
-		connection = self.get_connection()
-		channel = connection.channel()
-		channel.basic_publish(exchange='zapzap', routing_key='group_message_key', body=message)
-		connection.close()
-
-
-	def write_direct_message(self, recipient: str,  message: str):
-		connection = self.get_connection()
-		channel = connection.channel()
-		channel.basic_publish(exchange='zapzap', routing_key=recipient, body=message)
-		connection.close()
-	
-
-	def read_messages(self):
-		connection = self.get_connection()
-		channel = connection.channel()
-
-		def callback(ch, method, properties, body):
-			print(f"Inbox Message: {body}")
-
-		channel.basic_consume(queue=self.user_id, on_message_callback=callback, auto_ack=True)
-		channel.start_consuming()
-
 
 
 
